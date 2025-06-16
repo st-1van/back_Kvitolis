b936842e6170bb024662c63195b430c5fbba65b9
Ivan <st.ivan.k.k@gmail.com>
2025-06-16 17:25:15 +0300

    cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,7 +3,10 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000'],
+      origin: [
+        'http://localhost:3000',
+        process.env.HOST_FRONT
+      ].filter(Boolean),
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       headers: ['Content-Type', 'Authorization'],
     },
